32a0308a81b1b52cf9c149f843cbaa090991e596
oriolgomezdiaz <oriolgomezdiaz@users.noreply.github.com>
2025-11-22 23:09:21 +0000

    deploy: 48bed5176bc929c1667dc49bf71f01660c5f29dc

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-82ece057.js',
+                path_to_root + 'searchindex-2ba6d604.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
